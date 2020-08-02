862ae66c329ab2a77b813bf46edb084d74130028
smithc22atwit <44274496+smithc22atwit@users.noreply.github.com>
2020-08-02 00:06:48 -0400

    updated some files, finalized functionality of site

--- WebStructure3/config.php
+++ WebStructure3/config.php
@@ -5,6 +5,7 @@
 	define('DBPASS','wit123');
 
 	define("API_URL", "https://web-dev-final2020.herokuapp.com");
-	define("GET_SAMPLE_BY_QUESTION_TYPE", "/words/getSampleByQuestionType?question_type=")
+	define("GET_SAMPLE_BY_QUESTION_TYPE", "/words/getSampleByQuestionType?question_type=");
+	define("GET_ALL_WORDS", "/words/getAllWords");
 
 ?>
